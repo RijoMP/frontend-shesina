85dbd5b23a940056d6eb7ab1d942e6f5257b4f67
rijomp <rijomp99@gmail.com>
2022-09-14 01:21:08 +0530

    TEST

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import {useEffect,useState} from 'react'
+import CardCustom from "./Card"
+import { GridView } from '@mui/icons-material';
 
 
 // Generate Post Data
@@ -10,8 +14,38 @@ function preventDefault(event) {
 }
 
 export default function Posts() {
+  const [posts, setPost] = useState([]);
+  useEffect(() => {
+    let posts = []
+    fetch(`http://localhost:4675/posts/getposts`,
+    {
+      method: "POST",
+       
+      // Adding body or contents to send
+      body: JSON.stringify({
+          title: "foo",
+          body: "bar",
+          userId: 1
+      }),
+       
+      // Adding headers to the request
+      headers: {
+          "Content-type": "application/json; charset=UTF-8"
+      }})
+      .then((response) => response.json())
+      .then((response) => response.data)
+      .then((actualData) => setPost(actualData));
+  }, []);
   return (
-    <React.Fragment>
+    <React.Fragment><GridView>
+       <Typography gutterBottom variant="h5" component="div">
+        Posts
+      </Typography>
+      {posts.map((item,index)=>(
+        <CardCustom key={index} title ={item.title} description ={item.description} date ={item.date}/>
+      ))
+    }
+    </GridView>
      
     </React.Fragment>
   );
